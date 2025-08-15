9728c3fb38a2e2c06b85c8c14262e85e20424ff0
cleame8922 <cleame8922@sungkyul.ac.kr>
2025-08-15 22:13:39 +0900

    [fix]로그인, splash 화면 구현

--- App.tsx
+++ App.tsx
@@ -3,13 +3,17 @@ import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import Main from "src/screens/Main/Main";
 import PointMain from "src/screens/Point/PointMain";
+import Splash from "src/screens/Member/Splash";
+import Login from "src/screens/Member/Login";
 
 export default function App() {
   return (
     <NativeRouter>
       <Routes>
-        <Route path="/*" element={<PointMain />} />
+        <Route path="/*" element={<Login />} />
+        <Route path="/*" element={<Splash />} />
         <Route path="/*" element={<Main />} />
+        <Route path="/*" element={<PointMain />} />
       </Routes>
     </NativeRouter>
   );
