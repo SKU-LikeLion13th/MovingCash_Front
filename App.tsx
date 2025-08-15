4bc4adf737858b53af9475845eb4ab5e9c617777
이호연 <ihoyeon@ihoyeon-ui-MacBookAir.local>
2025-08-15 22:11:57 +0900

    [add]상점 메인 완

--- App.tsx
+++ App.tsx
@@ -1,16 +1,25 @@
-import { NativeRouter, Routes, Route } from "react-router-native";
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Main from "src/screens/Main/Main";
 import PointMain from "src/screens/Point/PointMain";
+import AvailablePoints from "src/screens/Point/AvailablePoints";
+
+export type RootStackParamList = {
+  PointMain: undefined;
+  Main: undefined;
+  AvailablePoints: undefined;
+};
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <NativeRouter>
-      <Routes>
-        <Route path="/*" element={<PointMain />} />
-        <Route path="/*" element={<Main />} />
-      </Routes>
-    </NativeRouter>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="PointMain" component={PointMain} />
+        <Stack.Screen name="Main" component={Main} />
+        <Stack.Screen name="AvailablePoints" component={AvailablePoints} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
