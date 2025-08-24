4be1cd4d140c7e5e797b9115f8ac00926661c81d
이호연 <ihoyeon@ihoyeon-ui-MacBookAir.local>
2025-08-24 15:43:44 +0900

    [fix]메인에 지도 불러오기, 최근무빙 소수점X

--- src/screens/Main/Main.tsx
+++ src/screens/Main/Main.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, ScrollView } from "react-native";
 import Profile from "./Profile";
 import WeeklyRunning from "./WeeklyRunning";
 import RecentMoving from "./RecentMoving";
 import StartNow from "./StartNow";
+import Map from "src/components/Map";
 
 export default function Main() {
   return (
-    <View className="h-full bg-[#101010] px-5">
+    <ScrollView className="h-full bg-[#101010] px-5">
       <View className="mt-16">
         <Profile />
       </View>
@@ -15,11 +16,14 @@ export default function Main() {
         <WeeklyRunning />
       </View>
       <View className="mt-8">
+        <Map />
+      </View>
+      <View className="mt-8">
         <RecentMoving />
       </View>
       <View className="mt-8">
         <StartNow />
       </View>
-    </View>
+    </ScrollView>
   );
 }
